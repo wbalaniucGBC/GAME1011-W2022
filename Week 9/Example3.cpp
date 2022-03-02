d4d1cdf108c07b2c2e1bad7888cadade39c02505
Wallace <Balaniuc>
2022-03-02 13:33:17 -0500

    [ADD] Example 4 - Using getline with text files

--- Week 9/Example3.cpp
+++ Week 9/Example3.cpp
@@ -4,45 +4,45 @@
 using namespace std;
 
 void showState(fstream&);
-
-int main()
-{
-	fstream testFile("stuff.dat", ios::out);
-	if (testFile.fail())
-	{
-		cout << "Cannot open the file.\n";
-		return 1;
-	}
-
-	// The file has opened successfully.
-	int num = 10;
-	cout << "Writing to the file.\n";
-	testFile << num;
-	showState(testFile);
-	testFile.close();
-
-	// Open the same file in input mode
-	testFile.open("stuff.dat", ios::in);
-	if (testFile.fail())
-	{
-		cout << "Cannot open the file.\n";
-	}
-	// Read the file
-	cout << "Reading from the file.\n";
-	testFile >> num;
-	showState(testFile);
-
-	// Are any error bits true? (Besides good) -- Yes! EOF bit
-
-	// Invalid read
-	cout << "Forcing a bad read operation.\n";
-	testFile >> num;
-	showState(testFile);
-
-	testFile.close();
-
-	return 0;
-}
+//
+//int main()
+//{
+//	fstream testFile("stuff.dat", ios::out);
+//	if (testFile.fail())
+//	{
+//		cout << "Cannot open the file.\n";
+//		return 1;
+//	}
+//
+//	// The file has opened successfully.
+//	int num = 10;
+//	cout << "Writing to the file.\n";
+//	testFile << num;
+//	showState(testFile);
+//	testFile.close();
+//
+//	// Open the same file in input mode
+//	testFile.open("stuff.dat", ios::in);
+//	if (testFile.fail())
+//	{
+//		cout << "Cannot open the file.\n";
+//	}
+//	// Read the file
+//	cout << "Reading from the file.\n";
+//	testFile >> num;
+//	showState(testFile);
+//
+//	// Are any error bits true? (Besides good) -- Yes! EOF bit
+//
+//	// Invalid read
+//	cout << "Forcing a bad read operation.\n";
+//	testFile >> num;
+//	showState(testFile);
+//
+//	testFile.close();
+//
+//	return 0;
+//}
 
 void showState(fstream& file)
 {
