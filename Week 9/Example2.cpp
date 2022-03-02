b0b9e78367b1eb8a272c9c04269af27e7e7c01df
Wallace <Balaniuc>
2022-03-02 12:53:23 -0500

    [ADD] Example 2 - ostringstream object demo.

---
+++ Week 9/Example2.cpp
@@ -0,0 +1,43 @@
+// Demonstrate the use of ostringstream object
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+using namespace std;
+
+// Function prototype
+string dollarFormat(double);
+
+int main()
+{
+	const int ROWS = 3, COLS = 2;
+
+	// 2D Array
+	double amount[ROWS][COLS] = {	184.5,	7, 
+									59.15,	64.32, 
+									7.29,	1289.1234 };
+
+	// Format table of dollar amount to be right justified and column widith of 10
+	cout << right;
+	for (int row = 0; row < ROWS; row++)
+	{
+		for (int column = 0; column < COLS; column++)
+		{
+			cout << setw(10) << dollarFormat(amount[row][column]);
+		}
+		cout << endl;
+	}
+
+	return 0;
+}
+
+string dollarFormat(double amount)
+{
+	// Create ostringstream object
+	ostringstream outStr;
+
+	// Set up format information and write to outStr
+	outStr << showpoint << fixed << setprecision(2);	// Forces 2 decimal places
+	outStr << '$' << amount;	// '$' + amount
+
+	return outStr.str();
+}
