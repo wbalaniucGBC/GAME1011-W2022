7a79741894281f7c44154543f4f1c5ba7a990c0f
Wallace <Balaniuc>
2022-03-02 13:23:33 -0500

    [ADD] Example 3 - File Stream Error Bits

--- Week 9/Example2.cpp
+++ Week 9/Example2.cpp
@@ -7,28 +7,28 @@ using namespace std;
 // Function prototype
 string dollarFormat(double);
 
-int main()
-{
-	const int ROWS = 3, COLS = 2;
-
-	// 2D Array
-	double amount[ROWS][COLS] = {	184.5,	7, 
-									59.15,	64.32, 
-									7.29,	1289.1234 };
-
-	// Format table of dollar amount to be right justified and column widith of 10
-	cout << right;
-	for (int row = 0; row < ROWS; row++)
-	{
-		for (int column = 0; column < COLS; column++)
-		{
-			cout << setw(10) << dollarFormat(amount[row][column]);
-		}
-		cout << endl;
-	}
-
-	return 0;
-}
+//int main()
+//{
+//	const int ROWS = 3, COLS = 2;
+//
+//	// 2D Array
+//	double amount[ROWS][COLS] = {	184.5,	7, 
+//									59.15,	64.32, 
+//									7.29,	1289.1234 };
+//
+//	// Format table of dollar amount to be right justified and column widith of 10
+//	cout << right;
+//	for (int row = 0; row < ROWS; row++)
+//	{
+//		for (int column = 0; column < COLS; column++)
+//		{
+//			cout << setw(10) << dollarFormat(amount[row][column]);
+//		}
+//		cout << endl;
+//	}
+//
+//	return 0;
+//}
 
 string dollarFormat(double amount)
 {
---
+++ Week 9/Example3.cpp
@@ -0,0 +1,55 @@
+// Demonstrate stream object error testing
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+void showState(fstream&);
+
+int main()
+{
+	fstream testFile("stuff.dat", ios::out);
+	if (testFile.fail())
+	{
+		cout << "Cannot open the file.\n";
+		return 1;
+	}
+
+	// The file has opened successfully.
+	int num = 10;
+	cout << "Writing to the file.\n";
+	testFile << num;
+	showState(testFile);
+	testFile.close();
+
+	// Open the same file in input mode
+	testFile.open("stuff.dat", ios::in);
+	if (testFile.fail())
+	{
+		cout << "Cannot open the file.\n";
+	}
+	// Read the file
+	cout << "Reading from the file.\n";
+	testFile >> num;
+	showState(testFile);
+
+	// Are any error bits true? (Besides good) -- Yes! EOF bit
+
+	// Invalid read
+	cout << "Forcing a bad read operation.\n";
+	testFile >> num;
+	showState(testFile);
+
+	testFile.close();
+
+	return 0;
+}
+
+void showState(fstream& file)
+{
+	cout << "File Status:\n";
+	cout << "eof bit: " << file.eof() << endl;		// End of file bit
+	cout << "fail bit: " << file.fail() << endl;	// Fail bit
+	cout << "bad bit: " << file.bad() << endl;		// Bad bit
+	cout << "good bit: " << file.good() << endl;	// Good bit
+	file.clear();
+}
